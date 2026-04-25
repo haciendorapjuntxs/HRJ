be9822eed18cddda1b62e314dc746aa98d1ff8ef
haciendorapjuntxs <iamarkteting@gmail.com>
2026-04-25 13:45:26 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', () => {
+    console.log("Sitio Haciendo Rap Juntxs cargado correctamente.");
+    
+    // Aquí puedes agregar más tarde el código para el carrusel 
+    // o animaciones de entrada al hacer scroll.
+});
